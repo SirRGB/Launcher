070ef8395ce7f360136abda543955d2710485f97
Liam <liam_wagner@gmx.de>
2021-08-30 14:16:09 +0200

    added button

--- app/src/main/java/com/sirrgb/launcher/MainActivity.kt
+++ app/src/main/java/com/sirrgb/launcher/MainActivity.kt
@@ -2,21 +2,25 @@ package com.sirrgb.launcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
-		calculateFibonacci()
+		calculateFibonacci(5)
 	}
 
-	fun calculateFibonacci() {
+	fun calculateFibonacci(maxCount: Int) {
 		var fibonacci = 1
 		var startwert = 0
 		var nachfolger = 1
 		var i = 0
-		while(i < 10) {
-			System.out.println(fibonacci)
+		var listOfFibonacci = arrayListOf(fibonacci)
+
+		while(i < maxCount) {
+			listOfFibonacci.add(fibonacci)
+			System.out.println(listOfFibonacci)
 			fibonacci = startwert + nachfolger
 			startwert = nachfolger
 			nachfolger = fibonacci
