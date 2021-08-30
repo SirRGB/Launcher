99084cd21b13608cd3edc0e4d4c1a8118021e0bb
itsragedev <mrmuffinzockt01@gmail.com>
2021-08-30 13:38:14 +0200

    Add Fibonacci calculation algorithm

--- app/src/main/java/com/sirrgb/launcher/MainActivity.kt
+++ app/src/main/java/com/sirrgb/launcher/MainActivity.kt
@@ -7,5 +7,20 @@ class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		calculateFibonacci()
+	}
+
+	fun calculateFibonacci() {
+		var fibonacci = 1
+		var startwert = 0
+		var nachfolger = 1
+		var i = 0
+		while(i < 10) {
+			System.out.println(fibonacci)
+			fibonacci = startwert + nachfolger
+			startwert = nachfolger
+			nachfolger = fibonacci
+			i++
+		}
 	}
 }
