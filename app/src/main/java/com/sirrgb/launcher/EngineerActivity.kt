ab02e1fad4b9d760c72e7f525bccfd3ba6c52d61
itsragedev <mrmuffinzockt01@gmail.com>
2021-08-31 14:24:53 +0200

    Add ListView, change intendation to tabs

--- app/src/main/java/com/sirrgb/launcher/EngineerActivity.kt
+++ app/src/main/java/com/sirrgb/launcher/EngineerActivity.kt
@@ -6,12 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 
 class EngineerActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_engineer)
-        //MESSAGE getIntent().getStringExtra("key")
-        //var extras = intent.getIntExtra()
-        var globalinput = intent.getStringExtra("test")
-        System.out.println(globalinput)
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_engineer)
+		//MESSAGE getIntent().getStringExtra("key")
+		//var extras = intent.getIntExtra()
+		var globalinput = intent.getStringExtra("test")
+		System.out.println(globalinput)
+	}
 }
